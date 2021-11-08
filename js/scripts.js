238f570f20dc8d0a60e49843fd3715afce2d8108
joel pallero <estebanjoelpallero@hotmail.com>
2021-11-08 02:42:38 -0300

    Se finaliza el Menú

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,21 @@ function open_menu(){
     body.classList.toggle("body_move");
     sidebar.classList.toggle("sidebar_move");
 }
+
+//Menu responsive
+
+if(window.innerWidth < 760){
+    body.classList.add("body_move")
+    sidebar.classList.add("sidebar_move")
+}
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth > 760){
+        body.classList.remove("body_move");
+        sidebar.classList.remove("sidebar_move");
+    }
+    if(window.innerWidth < 760){
+        body.classList.add("body_move");
+        sidebar.classList.add("sidebar_move");
+    }
+});
